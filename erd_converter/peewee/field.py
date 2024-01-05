b5974bc88a4047fe7158351710f1d5463a521d9b
dzanadvo <dzanadvo@its.jnj.com>
2024-01-05 19:44:23 +0400

    fix peewee

--- erd_converter/peewee/field.py
+++ erd_converter/peewee/field.py
@@ -1,63 +1,112 @@
 from __future__ import annotations
 
 import dataclasses
+import typing as tp
 
-from typing_extensions import Self
-
-from erd_converter.base import BaseField, Field
+from erd_converter.base import BaseField, Field, IntegerField, VarcharField
 from .utils import get_peewee_table_class_name
 
 
+# @dataclasses.dataclass
+# class PeeweeField(BaseField):
+#     name: str
+#     type: str
+#     primary_key: bool = False
+#     nullable: bool = False
+#     reference: str | None = None
+
+#     def to_field(self) -> Field:
+#         return NotImplemented()
+
+#     @classmethod
+#     def from_field(cls, field: Field) -> Self:
+#         return cls(
+#             name=field.name,
+#             type=field.type,
+#             primary_key=field.primary_key,
+#             nullable=field.nullable,
+#             reference=field.reference,
+#         )
+
+#     def get_peewee_type(self) -> str:
+#         if self.reference:
+#             return 'ForeignKeyField'
+#         if self.type == 'int':
+#             if self.primary_key:
+#                 return 'AutoField'
+#             return 'IntegerField'
+#         if self.type == 'varchar':
+#             return 'CharField'
+#         if self.type == 'json':
+#             return 'JSONField'
+#         if self.type == 'boolean':
+#             return 'BooleanField'
+
+#     def get_options(self) -> str:
+#         options = []
+#         if self.nullable:
+#             options.append('null=True')
+#         return ','.join(options)
+
+#     def get_peewee_field_definition(self) -> str:
+#         field_type = self.get_peewee_type()
+#         if field_type == 'ForeignKeyField':
+#             ref_table, ref_field = self.reference.split('.')
+#             ref_table_name = get_peewee_table_class_name(ref_table)
+#             op = f"field='{ref_field}', lazy_load=False, " + op
+#             return f'ForeignKeyField({ref_table_name}, {op})'
+
+#         return f'{field_type}({op})'
+
+#     def __str__(self) -> str:
+#         field_definition = self.get_peewee_field_definition()
+#         return f'{self.name} = {field_definition}'
+
+
+T = tp.TypeVar('T')
+
+
+class BasePeeweeField(BaseField[T]):
+    pass
+
+
 @dataclasses.dataclass
-class PeeweeField(BaseField):
+class PeeweeIntegerField(BasePeeweeField[IntegerField]):
     name: str
-    type: str
     primary_key: bool = False
     nullable: bool = False
-    reference: str | None = None
 
-    def to_field(self) -> Field:
-        return NotImplemented()
-
-    @classmethod
-    def from_field(cls, field: Field) -> Self:
-        return cls(
-            name=field.name,
-            type=field.type,
-            primary_key=field.primary_key,
-            nullable=field.nullable,
-            reference=field.reference,
-        )
-
-    def get_peewee_type(self) -> str:
-        if self.reference:
-            return 'ForeignKeyField'
-        if self.type == 'int':
-            if self.primary_key:
-                return 'AutoField'
-            return 'IntegerField'
-        if self.type == 'varchar':
-            return 'CharField'
-        if self.type == 'json':
-            return 'JSONField'
-        if self.type == 'boolean':
-            return 'BooleanField'
-
-    def get_peewee_field_definition(self) -> str:
-        field_type = self.get_peewee_type()
+    @property
+    def options(self) -> str:
         options = []
-
         if self.nullable:
             options.append('null=True')
-        op = ','.join(options)
-        if field_type == 'ForeignKeyField':
-            ref_table, ref_field = self.reference.split('.')
-            ref_table_name = get_peewee_table_class_name(ref_table)
-            op = f"field='{ref_field}', lazy_load=False, " + op
-            return f'ForeignKeyField({ref_table_name}, {op})'
-
-        return f'{field_type}({op})'
+        return ','.join(options)
 
     def __str__(self) -> str:
-        field_definition = self.get_peewee_field_definition()
-        return f'{self.name} = {field_definition}'
+        if not self.nullable and self.primary_key:
+            field_type = 'AutoField'
+        else:
+            field_type = 'IntegerField'
+        return f'{self.name} = {field_type}({self.options})'
+
+
+@dataclasses.dataclass
+class PeeweeVarcharField(BasePeeweeField[VarcharField]):
+    name: str
+    size: int
+    primary_key: bool = False
+    nullable: bool = False
+
+    @property
+    def options(self) -> str:
+        options = []
+        if self.size != 256:
+            options.append(f'max_length={self.size}')
+        if self.nullable:
+            options.append('null=True')
+        return ', '.join(options)
+
+    def __str__(self) -> str:
+        return f'{self.name} = CharField({self.options})'
+
--- erd_converter/peewee/table.py
+++ erd_converter/peewee/table.py
@@ -1,25 +1,34 @@
 from __future__ import annotations
 
 import dataclasses
+import typing as tp
 
 from typing_extensions import Self
 
-from erd_converter.base import BaseTable, Table
-from .field import PeeweeField
+from erd_converter.base import BaseTable, Table, IntegerField, VarcharField
+from .field import BasePeeweeField, PeeweeIntegerField, PeeweeVarcharField
 from .utils import get_peewee_table_class_name
 
 
 @dataclasses.dataclass
 class PeeweeTable(BaseTable):
     name: str
-    fields: list[PeeweeField] = dataclasses.field(default_factory=lambda: [])
+    fields: list[BasePeeweeField] = dataclasses.field(default_factory=lambda: [])
+    field_convert: tp.ClassVar[dict] = {
+        IntegerField: PeeweeIntegerField,
+        VarcharField: PeeweeVarcharField,
+    }
 
     def to_table(self) -> Table:
         return Table(name=self.name, fields=[field.to_field() for field in self.fields])
 
     @classmethod
     def from_table(cls, table: Table) -> Self:
-        return cls(name=table.name, fields=[PeeweeField.from_field(field) for field in table.fields])
+        fields = []
+        for field in table.fields:
+            f = cls.field_convert[field.__class__].from_field(field)
+            fields.append(f)
+        return cls(name=table.name, fields=fields)
 
     def __str__(self) -> str:
         table_name = get_peewee_table_class_name(self.name)
